3e3169821aa8f9f7173827ffc00482299b016d63
KushibikiMashu <kushibiki3413@gmail.com>
2019-05-18 21:29:36 +0900

    append.goなど8ファイルを追加

---
+++ src/structs_slices_and_maps/making-slices.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func printSlice(s string, x []int) {
+	fmt.Printf("%s len=%d cap=%d %v\n", s, len(x), cap(x), x)
+}
+
+func main()  {
+	// スライスは、組み込みの make 関数を使用して作成することができます。 
+	// これは、動的サイズの配列を作成する方法です。
+	// make(slice, len, cap)
+	a := make([]int, 5)
+	printSlice("a", a) // a len=5 cap=5 [0 0 0 0 0]
+
+	b := make([]int, 0, 5)
+	printSlice("b", b) // b len=0 cap=5 []
+
+	c := b[:2]
+	printSlice("c", c) // c len=2 cap=5 [0 0]
+
+	d := c[2:5]
+	printSlice("d", d) // d len=3 cap=3 [0 0 0]
+}
