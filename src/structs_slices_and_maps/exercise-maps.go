0702072e8b09ae490e1427f064cd6fcbbcb13ef4
KushibikiMashu <kushibiki3413@gmail.com>
2019-05-19 00:37:05 +0900

    maps.goなど8ファイルを追加

---
+++ src/structs_slices_and_maps/exercise-maps.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"golang.org/x/tour/wc"
+	"strings"
+)
+
+func WordCount(s string) map[string]int {
+	words := strings.Fields(s)
+	m := make(map[string]int)
+	
+	for _, w := range words {
+		m[w] += 1
+	}
+	
+	return m
+}
+
+func main() {
+	wc.Test(WordCount)
+}
---
+++ src/structs_slices_and_maps/mutating-maps.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main()  {
+	m := make(map[string]int)
+
+	m["Answer"] = 42
+	fmt.Println("The value:", m["Answer"])
+	
+	m["Answer"] = 48
+	fmt.Println("The value:", m["Answer"])
+
+	delete(m, "Answer")
+	fmt.Println("The value:", m["Answer"])
+
+	// もし、 m に key があれば、変数 ok は true となり、存在しなければ、 ok は false となります。
+	v, ok := m["Answer"]
+	fmt.Println("The value:", v, "Present?", ok)
+
+	m["Answer"] = 40
+	v, ok = m["Answer"]
+	fmt.Println("The value:", v, "Present?", ok)
+
+}
